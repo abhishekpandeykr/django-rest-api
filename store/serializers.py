29e3f9583deeb794c70db2f23366c111f24fd5d3
karma007 <abhishekpandey.kr@gmail.com>
2022-03-07 09:53:07 +0530

    renaming serializers and custom field for serializer

--- store/serializers.py
+++ store/serializers.py
@@ -1,6 +1,13 @@
+from decimal import Decimal
 from rest_framework import serializers
+
+from store.models import Product
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(max_length=255)
-    price_per_unit = serializers.DecimalField(max_digits=10, decimal_places=2)
+    price = serializers.DecimalField(max_digits=10, decimal_places=2, source='price_per_unit')
+    price_with_tax = serializers.SerializerMethodField(method_name='get_price_with_tax')
+
+    def get_price_with_tax(self, product:Product):
+        return product.price_per_unit * Decimal(1.1)
