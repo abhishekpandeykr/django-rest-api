20572a8a5ca17dd7a73598200133e607b564feff
karma007 <abhishekpandey.kr@gmail.com>
2022-03-07 19:04:22 +0530

    saving a product information added

--- store/serializers.py
+++ store/serializers.py
@@ -1,3 +1,4 @@
+from dataclasses import fields
 from decimal import Decimal
 from rest_framework import serializers
 
@@ -7,15 +8,14 @@ class CollectionSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(max_length=255)
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=10, decimal_places=2, source='price_per_unit')
-    price_with_tax = serializers.SerializerMethodField(method_name='get_price_with_tax')
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description', 'slug', 'price_per_unit', 'collection']
+    # price_with_tax = serializers.SerializerMethodField(method_name='get_price_with_tax')
     # collection = serializers.PrimaryKeyRelatedField(queryset=Collection.objects.all(), required=False)
     # aceess the collection string
     # collection = serializers.StringRelatedField(read_only=True)
-    collection = CollectionSerializer(read_only=True)
 
     def get_price_with_tax(self, product:Product):
         return product.price_per_unit * Decimal(1.1)
--- store/views.py
+++ store/views.py
@@ -9,11 +9,17 @@ from .serializers import ProductSerializer
 # Create your views here.
 
 # create product list view
-@api_view()
+@api_view(['GET', 'POST'])
 def product_list(request):
-    product_query_set = Product.objects.all()
-    serializer = ProductSerializer(product_query_set, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        product_query_set = Product.objects.all()
+        serializer = ProductSerializer(product_query_set, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 @api_view()
 def product_detail(request, pk):
