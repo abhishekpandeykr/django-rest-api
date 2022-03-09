82af381053b8d89f8b3012748a98619fbb8e10c1
karma007 <abhishekpandey.kr@gmail.com>
2022-03-09 13:27:42 +0530

    viewset added for collection label

--- testapp/urls.py
+++ testapp/urls.py
@@ -1,9 +1,10 @@
 #  import pathlib
 # from django.urls import path, include
 from rest_framework.routers import DefaultRouter, SimpleRouter
-from .views import ProductViewSets
+from .views import CollectionViewSets, ProductViewSets
 
 custom_router = SimpleRouter()
 custom_router.register('products', ProductViewSets)
+custom_router.register('collection', CollectionViewSets)
 
 urlpatterns = custom_router.urls
--- testapp/views.py
+++ testapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from store.serializers import ProductSerializer
-from store.models import Product
+from store.serializers import ProductSerializer, CollectionSerializer
+from store.models import Product, Collection
 from rest_framework.viewsets import ModelViewSet
 # Create your views here.
 
@@ -10,4 +10,9 @@ class ProductViewSets(ModelViewSet):
 
     def get_serializer_context(self):
         return {'request':self.request}
+
+
+class CollectionViewSets(ModelViewSet):
+    queryset = Collection.objects.all()
+    serializer_class = CollectionSerializer
 
