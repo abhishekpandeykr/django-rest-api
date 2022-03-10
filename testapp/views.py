f9298a90d39e8711589080b07911d3f929b3f1b2
karma007 <abhishekpandey.kr@gmail.com>
2022-03-10 10:39:53 +0530

    pagination implemented for the api

--- testapp/views.py
+++ testapp/views.py
@@ -3,17 +3,22 @@ from store.serializers import ProductSerializer, CollectionSerializer
 from store.models import Product, Collection
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from testapp.models import Reviews
 from testapp.serializers import ReviewSerializers
 from .filter import ProductFilter
+from .pagination import DefaultPagination
 # Create your views here.
 
 class ProductViewSets(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
+    search_fields = ['title', 'description']
+    ordering_fields = ['price_per_unit', 'created_at']
+    pagination_class = DefaultPagination
 
     def get_serializer_context(self):
         return {'request':self.request}
