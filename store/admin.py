7c886163e4c6bd29186ae8633ac767cf05d9a370
karma007 <abhishekpandey.kr@gmail.com>
2022-03-14 23:10:28 +0530

    extending user object

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,6 @@
+from asyncio import constants
 from django.contrib import admin
-from .models import Product, Collection
+from .models import Product, Collection, Employee
 
 class CustomProductAdmin(admin.ModelAdmin):
     list_display = ('title', 'price_per_unit', 'slug')
@@ -10,6 +11,18 @@ class CustomProductAdmin(admin.ModelAdmin):
 class CustomCollectionAdmin(admin.ModelAdmin):
     list_display = ('title', 'feature_product', 'id')
 
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'job_type']
+    # list_display = ['user__first_name', 'user__last_name', 'phone', 'job_type']
+
+    def first_name(self, obj):
+        return obj
+    
+    def last_name(self, obj):
+        return obj
+
 admin.site.register(Product, CustomProductAdmin)
 admin.site.register(Collection, CustomCollectionAdmin)
+admin.site.register(Employee, EmployeeAdmin)
 # Register your models here.
--- store/models.py
+++ store/models.py
@@ -1,6 +1,5 @@
-from turtle import title
-from unicodedata import category
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Product(models.Model):
@@ -26,3 +25,21 @@ class Collection(models.Model):
     
     class Meta:
         ordering = ('title',)
+
+
+class Employee(models.Model):
+    FRONTEND_DEVELOPER = 'FE'
+    BACKEND_DEVELOPER = 'BE'
+    FULLSTACK_DEVELOPER = 'FS'
+
+    JOB_CHOICES = [(FRONTEND_DEVELOPER, 'Frontend Developer'), (BACKEND_DEVELOPER, 'Backend Developer'),
+                     (FULLSTACK_DEVELOPER, 'Fullstack Developer')]
+    phone = models.CharField(max_length=10)
+    job_type = models.CharField(max_length=2, choices=JOB_CHOICES, default=FRONTEND_DEVELOPER)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.user.first_name} {self.user.last_name}"
+    
+    class Meta:
+        ordering = ['user__first_name', 'user__last_name']
